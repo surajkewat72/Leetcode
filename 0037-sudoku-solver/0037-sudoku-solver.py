4d8dd8841211e6b41b7ef6a498aa4adab05f9f85
Fumetsu <surajkewat7278@gmail.com>
2025-08-31 18:43:25 +0530

    Time: 2157 ms (42.37%), Space: 18.1 MB (39.52%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,50 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empties = []
+
+        # Initialize sets with existing numbers
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    empties.append((r, c))
+                else:
+                    num = board[r][c]
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[(r // 3) * 3 + (c // 3)].add(num)
+
+        def backtrack(i=0):
+            if i == len(empties):
+                return True  # Solved
+            
+            r, c = empties[i]
+            b = (r // 3) * 3 + (c // 3)
+
+            for ch in map(str, range(1, 10)):
+                if ch not in rows[r] and ch not in cols[c] and ch not in boxes[b]:
+                    # Place number
+                    board[r][c] = ch
+                    rows[r].add(ch)
+                    cols[c].add(ch)
+                    boxes[b].add(ch)
+
+                    if backtrack(i + 1):
+                        return True  # Success
+
+                    # Undo (backtrack)
+                    board[r][c] = '.'
+                    rows[r].remove(ch)
+                    cols[c].remove(ch)
+                    boxes[b].remove(ch)
+
+            return False
+
+        backtrack()
+        return board
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
