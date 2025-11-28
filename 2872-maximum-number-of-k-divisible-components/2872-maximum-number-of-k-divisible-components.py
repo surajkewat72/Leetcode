7d8d8eb6f1139c892381d489a8db69b9b5de6ee7
Fumetsu <surajkewat7278@gmail.com>
2025-11-28 13:15:24 +0530

    Time: 109 ms (79.71%), Space: 35.8 MB (80.44%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
@@ -7,7 +7,7 @@ class Solution:
             adj_list[node1].append(node2)
             adj_list[node2].append(node1)
 
-        component_count = [0]  # Use a list to pass by reference
+        component_count = [0]  
 
         self.dfs(0, -1, adj_list, values, k, component_count)
 
@@ -26,7 +26,6 @@ class Solution:
 
         for neighbor_node in adj_list[current_node]:
             if neighbor_node != parent_node:
-                # Recursive call to process the subtree rooted at the neighbor
                 sum_ += self.dfs(
                     neighbor_node,
                     current_node,
@@ -35,15 +34,12 @@ class Solution:
                     k,
                     component_count,
                 )
-                sum_ %= k  # Ensure the sum stays within bounds
+                sum_ %= k  
 
-        # Step 3: Add the value of the current node to the sum
         sum_ += node_values[current_node]
         sum_ %= k
 
-        # Step 4: Check if the sum is divisible by k
         if sum_ == 0:
             component_count[0] += 1
 
-        # Step 5: Return the computed sum for the current subtree
         return sum_
