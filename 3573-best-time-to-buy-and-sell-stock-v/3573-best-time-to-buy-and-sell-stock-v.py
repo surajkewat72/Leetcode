1ca2ebbff98e0d293a23a5da5168d63fb11343eb
Fumetsu <surajkewat7278@gmail.com>
2025-12-17 11:55:43 +0530

    Time: 2140 ms (47.23%), Space: 18.1 MB (88%) - LeetHub

---
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumProfit(self, prices: List[int], k: int) -> int:
+        INF = float('-inf')
+        
+        dp = [[INF] * 3 for _ in range(k + 1)]
+        dp[0][0] = 0  
+
+        for price in prices:
+            new_dp = [row[:] for row in dp]
+
+            for t in range(k + 1):
+                new_dp[t][1] = max(new_dp[t][1], dp[t][0] - price)
+                new_dp[t][2] = max(new_dp[t][2], dp[t][0] + price)
+
+                if t < k:
+                    new_dp[t + 1][0] = max(new_dp[t + 1][0], dp[t][1] + price)
+                    new_dp[t + 1][0] = max(new_dp[t + 1][0], dp[t][2] - price)
+
+            dp = new_dp
+
+        return max(dp[t][0] for t in range(k + 1))
+            
